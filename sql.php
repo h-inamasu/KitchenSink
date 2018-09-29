f3f64875f5c251fc1315d5c984ca692a5f320397
Hideyuki Inamasu <h-inamasu@1onepiece.jp>
2018-09-28 19:31:51 -0700

    sql

--- sql.php
+++ sql.php
@@ -2,10 +2,12 @@
 
 $url=parse_url(getenv('DATABASE_URL'));
 
-$text="host name: ".$url['host']."\n";
-print $text;
-print "aaa";
-//print(substr($url['path'],1));
-//print($url['user']);
-//print($url['pass']);
+$text="host name: ".$url['host']";
+error_log($text);
+$text="path: ".substr($url['path'],1);
+error_log($text);
+$text="user: ".$url['user'];
+error_log($text);
+$text="password: ".$url['pass'];
+error_log($text);
 ?>
