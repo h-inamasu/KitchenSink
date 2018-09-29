70c3ed4b8f00d7ea090de89a51ff66ed57d38b56
Hideyuki Inamasu <h-inamasu@1onepiece.jp>
2018-09-28 19:33:13 -0700

    sql

--- sql.php
+++ sql.php
@@ -2,7 +2,7 @@
 
 $url=parse_url(getenv('DATABASE_URL'));
 
-$text="host name: ".$url['host']";
+$text="host name: ".$url['host'];
 error_log($text);
 $text="path: ".substr($url['path'],1);
 error_log($text);
