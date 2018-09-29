36e1bfa069c092b5a98499457e551a10bf6d143c
Hideyuki Inamasu <h-inamasu@1onepiece.jp>
2018-09-28 19:35:26 -0700

    sql

--- sql.php
+++ sql.php
@@ -6,6 +6,8 @@ $text="host name: ".$url['host'];
 error_log($text);
 $text="path: ".substr($url['path'],1);
 error_log($text);
+$text="path: ".$url['path'];
+error_log($text);
 $text="user: ".$url['user'];
 error_log($text);
 $text="password: ".$url['pass'];
