608092b3fa73eda7978bb94842339463ed87cd2a
Hideyuki Inamasu <h-inamasu@1onepiece.jp>
2018-09-28 19:28:09 -0700

    sql

--- sql.php
+++ sql.php
@@ -2,7 +2,7 @@
 
 $url=parse_url(getenv('DATABASE_URL'));
 
-$text='host name: ".$url['host']."\n";
+$text="host name: ".$url['host']."\n";
 print $text;
 print "aaa";
 //print(substr($url['path'],1));
