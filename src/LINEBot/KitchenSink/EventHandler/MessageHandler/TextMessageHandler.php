9e8f449a0b9a45d1446186f02a92364dd0767b5d
Hideyuki Inamasu <h-inamasu@1onepiece.jp>
2018-09-29 20:10:32 -0700

    test

--- src/LINEBot/KitchenSink/EventHandler/MessageHandler/TextMessageHandler.php
+++ src/LINEBot/KitchenSink/EventHandler/MessageHandler/TextMessageHandler.php
@@ -84,10 +84,10 @@ class TextMessageHandler implements EventHandler
         switch ($text) {
             case 'sql':
 error_log("----- SQL");
-                $stmt=$this->bot->pdo->query("select * from pg_user;");
                 if ($this->bot->pdo==null) {
 error_log("null null null null null");
                 }
+                $stmt=$this->bot->pdo->query("select * from pg_user;");
 error_log("0000000000");
                 $users=$stmt->fetchAll();
 error_log("1111111111");
